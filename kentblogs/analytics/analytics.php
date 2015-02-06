a7e099ec107d37cd2c6d929de9d738245de22977
gjmh-unikent <g.haddow@kent.ac.uk>
2015-02-06 09:46:02 +0000

    add central ga tracking

--- kentblogs/analytics/analytics.php
+++ kentblogs/analytics/analytics.php
@@ -12,10 +12,14 @@ echo "<script type='text/javascript'>
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-21420000-13', 'auto');
-    ga('require', 'displayfeatures');
-    ga('require', 'linkid', 'linkid.js');
-    ga('send', 'pageview');
+    ga('blogs.create', 'UA-21420000-13', 'auto');
+    ga('blogs.require', 'displayfeatures');
+    ga('blogs.require', 'linkid', 'linkid.js');
+    ga('blogs.send', 'pageview');
+    ga('central.create', 'UA-54179016-1', 'auto');
+    ga('central.require', 'displayfeatures');
+    ga('central.require', 'linkid', 'linkid.js');
+    ga('central.send', 'pageview');
 
 </script>";
 
