17564662ecea618924ac046e074150f7af01c1e1
gjmh-unikent <g.haddow@kent.ac.uk>
2015-02-23 15:41:26 +0000

    - fix aggregator rebuild on blog delete/archive/deactivate - add opt-out option for aggregator - add default aggregator image option - fix for kent nav bar on twentyfifteen theme effecting sidebar

--- kentblogs/aggregator/aggregator.php
+++ kentblogs/aggregator/aggregator.php
@@ -1,6 +1,100 @@
 <?php 
 
 include 'helper.php';
+include '/../vendor/MM_MediaAccess.php';
+
+$kb_aggregator_media_access = new HaddowG\MetaMaterial\MM_MediaAccess(array(
+    'name'=>'fdefault',
+    'file_type'=>'image',
+    'buttonText'=>'Select Image',
+    'modal_button_text'=>'Select Image',
+    'modal_title'=>'Choose a Default Image',
+    'placeholderImg'=>'/wp-content/mu-plugins/kentblogs/aggregator/featured_default.png'));
 
 // This is run every time a post is saved/updated
 add_action('save_post', 'kentblogs_aggregator_post_saved');
+
+add_action("archive_blog", "kentblogs_aggregator_init_posts");
+add_action("delete_blog", "kentblogs_aggregator_init_posts");
+add_action("deactivate_blog", "kentblogs_aggregator_init_posts");
+
+
+function kentblogs_aggregator_options_menu() {
+    if (function_exists('add_submenu_page') && is_admin()) {
+        add_submenu_page('options-general.php', 'Aggregator Options', 'Blogs Aggregator','manage_options', basename(__FILE__), 'kentblogs_aggregator_options');
+    }
+}
+add_action('admin_menu','kentblogs_aggregator_options_menu');
+
+function kentblogs_aggregator_options(){
+    global $kb_aggregator_media_access;
+    $kb_aggregator_media_access->setGroupName('fdefault');
+
+    $ex = get_option('kb_exclude_from_aggregator');
+    $img = get_option('kb_default_aggregator_img');
+
+    if (isset($_POST['update_aggregator'])) {
+        if(isset($_POST['kb_exclude_from_aggregator'])){
+            update_option('kb_exclude_from_aggregator',$_POST['kb_exclude_from_aggregator']);
+            $changed = (((!empty($ex)) && ($ex!==$_POST['kb_exclude_from_aggregator'])) || (empty($ex) && (!empty($_POST['kb_exclude_from_aggregator']))));
+            $ex='excluded';
+        }else{
+            delete_option('kb_exclude_from_aggregator');
+            $changed = (!empty($ex));
+            $ex=false;
+        }
+
+        if(isset($_POST['kb_default_aggregator_img'])) {
+            $changed= $changed || (((!empty($img)) && ($img!==$_POST['kb_default_aggregator_img'])) ||( empty($img) && (!empty($_POST['kb_default_aggregator_img']))))  ;
+
+            update_option('kb_default_aggregator_img', $_POST['kb_default_aggregator_img']);
+            $img =  $_POST['kb_default_aggregator_img'];
+        }
+        if($changed) {
+            kentblogs_aggregator_init_posts();
+        }
+    }
+
+    ?>
+    <script>
+        jQuery(document).ready(function($){
+           $('#kb_agg_clear_img').click(function(e){e.preventDefault();
+           $('.media-field-id-fdefault').val('');
+           $img = $('.media-field-thumb-fdefault').first();
+               $img.attr('src',$img.data('placeholder'));
+           });
+        });
+    </script>
+    <div class=wrap>
+        <h2>Blogs Aggregator Options</h2>
+        <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+            <table class="form-table">
+                <tbody>
+                <tr>
+                    <th>
+                        <label for="kb_exclude_from_aggregator">Exclude this Blog</label>
+                    </th>
+                    <td>
+                        <input type="checkbox" id="kb_exclude_from_aggregator" name="kb_exclude_from_aggregator" value="exclude"<?php echo (!empty($ex)?' checked="checked"':''); ?>>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="kb_default_aggregator_img">Default Featured Image</label>
+                    </th>
+                    <td>
+                        <?php
+                            echo $kb_aggregator_media_access->getField($img,'kb_default_aggregator_img');
+                            if(!empty($img)){
+                                echo '<button class="button" id="kb_agg_clear_img">Clear Image</button>';
+                            }
+                        ?>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <p class="submit"><input type="submit" value="Save Changes" class="button button-primary" id="submit" name="update_aggregator"></p>
+        </form>
+    </div>
+<?php
+}
--- kentblogs/aggregator/helper.php
+++ kentblogs/aggregator/helper.php
@@ -49,22 +49,32 @@ function kentblogs_aggregator_init_posts(){
 		// get last month's posts
 		switch_to_blog($blog['blog_id']);
 
-		$posts = get_posts(
-			array (
-				'date_query' => array(
-					'after' => '-2 months'
-				),
-				'posts_per_page' => 5
-			)
-		);
+        $excluded = get_option('kb_exclude_from_aggregator');
+        if(empty($excluded)) {
 
-		if (!empty($posts)) {
-			foreach ($posts as $post) {
-				$post = kentblogs_aggregator_format_post($post, $blog['blog_id']);
-				if(!empty($post)) $aggregate_data[$blog['blog_id'].'_'.$post['id']] = $post;
-			}
-		}
+            $args = array(
+                'date_query' => array(
+                    'after' => '-2 months'
+                ),
+                'posts_per_page' => 5
+            );
 
+            //fix for local/dev database being old
+            if(WP_ENV!=='production'){
+                $args['date_query'] = array(
+                    'after' => '-6 months'
+                );
+            }
+
+            $posts = get_posts($args);
+
+            if (!empty($posts)) {
+                foreach ($posts as $post) {
+                    $post = kentblogs_aggregator_format_post($post, $blog['blog_id']);
+                    if (!empty($post)) $aggregate_data[$blog['blog_id'] . '_' . $post['id']] = $post;
+                }
+            }
+        }
 		restore_current_blog();
 	}
 
@@ -96,9 +106,10 @@ function kentblogs_aggregator_format_post($post, $blog){
 	if($post->status !== 'publish' || $post->type !== 'post') return false;
 	$featured_image = isset($post->featured_image) && !empty($post->featured_image) ? $post->featured_image : isset($post->media[0]) ? $post->media[0] : array();
 	
-	if (empty($featured_image)) return false;
+	if (empty($featured_image)){
+		$featured_image  = kentblogs_get_aggregator_default_image();
+	}
 
-	
 	return array(
 		'id' => $post->id,
 		'title' => $post->title,
@@ -144,26 +155,29 @@ function kentblogs_aggregator_sort_and_trim_posts($posts){
 	return array_slice($posts, 0, 60);
 }
 
+function kentblogs_get_aggregator_default_image(){
+
+	$img =  get_option('kb_default_aggregator_img');
 
 
-function kentblogs_remove_from_aggregator($id){
+	if(empty($img)) {
+		$img = 'http://blogs.kent.ac.uk/wp-content/mu-plugins/kentblogs/aggregator/featured_default.png';
+	}else{
+        $img = wp_get_attachment_image_src($img,'large',false);
+        $img = $img[0];
+    }
 
-	$aggregate_data = get_site_option('wp-multisite-post-aggregate');
-
-	if($aggregate_data === false) {
-		return;
-	}
-
-	foreach(array_keys($aggregate_data) as $key ){
-		$keyparts = explode('_',$key);
-
-		if($keyparts[0]==$id){
-			unset($aggregate_data[$key]);
-		}
-	}
-	update_site_option('wp-multisite-post-aggregate', $aggregate_data);
+	return array(
+		'id' => 0,
+		'slug' => 'featured-default',
+		'description' => '',
+		'caption' => '',
+		'title' => '',
+		'mime_type' => 'image/png',
+		'alt_text' =>'',
+		'sizes' => array('large'=>array('url'=>$img),
+			'medium'=>array('url'=>$img),
+			'thumbnail'=>array('url'=>$img),
+			'post_thumbnail'=>array('url'=>$img))
+	);
 }
-
-add_action("archive_blog", "kentblogs_remove_from_aggregator");
-add_action("delete_blog", "kentblogs_remove_from_aggregator");
-add_action("deactivate_blog", "kentblogs_remove_from_aggregator");
