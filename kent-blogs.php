4df9d0f1a444eec08b09521d29f1600e4914bbef
gjmh-unikent <g.haddow@kent.ac.uk>
2015-04-22 10:55:28 +0100

    disable report concern

--- kent-blogs.php
+++ kent-blogs.php
@@ -4,7 +4,7 @@ include_once('kentblogs/cron/cron-scheduler.php');
 include_once('kentblogs/kent-nav-bar/kent-nav-bar.php');
 include_once('kentblogs/login-form/login-form.php');
 include_once('kentblogs/analytics/analytics.php');
-include_once('kentblogs/report-concern/report-concern.php');
+//include_once('kentblogs/report-concern/report-concern.php');
 include_once('kentblogs/subheadings/subheadings.php');
 include_once('kentblogs/blogs-footer/blogs-footer.php');
 include_once('kentblogs/aggregator/aggregator.php');
